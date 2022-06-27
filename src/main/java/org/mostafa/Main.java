27232ada28fd47703064cd79d07cd88cc105e62f
mostafa4099 <mostafa.sna@gmail.com>
2022-06-27 11:14:26 +0600

    First Commit

---
+++ src/main/java/org/mostafa/Main.java
@@ -0,0 +1,12 @@
+package org.mostafa;
+
+/**
+ * @author Md. Golam Mostafa | mostafa.sna@gmail.com
+ * @File org.mostafa.${NAME}.java: ${PROJECT_NAME}
+ * @CreationDate ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
