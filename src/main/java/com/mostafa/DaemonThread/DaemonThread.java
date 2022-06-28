9c21aacfde3f92d7d59d63f2df8d9251d9660b0d
mostafa4099 <mostafa.sna@gmail.com>
2022-06-28 14:28:44 +0600

    Daemon Thread Added

---
+++ src/main/java/com/mostafa/DaemonThread/DaemonThread.java
@@ -0,0 +1,27 @@
+package com.mostafa.DaemonThread;
+
+/**
+ * @author Md. Golam Mostafa | mostafa.sna@gmail.com
+ * @File com.mostafa.ExecutionScheduling.ExecutionScheduling.java: JavaParallelProgramming
+ * @CreationDate 6/27/2022 5:11 PM
+ */
+public class DaemonThread {
+    public static void main(String[] args) throws Exception {
+        Thread cleaner = new KitchenCleaner();
+        // Daemon thread in Java is a low-priority thread
+        // that runs in the background to perform tasks such as garbage collection.
+        // Daemon thread in Java is also a service provider thread that provides services to the user thread.
+        // When all the user threads die, JVM terminates this thread automatically.
+        // To make a thread as daemon thread need to set attribute daemon is true
+        cleaner.setDaemon(true);
+        cleaner.start();
+
+        System.out.println("Main Chef is cooking...");
+        Thread.sleep(600);
+        System.out.println("Main Chef is cooking...");
+        Thread.sleep(600);
+        System.out.println("Main Chef is cooking...");
+        Thread.sleep(600);
+        System.out.println("Main Chef is done!");
+    }
+}
---
+++ src/main/java/com/mostafa/Runnable/Chef.java
@@ -0,0 +1,19 @@
+package com.mostafa.Runnable;
+
+/**
+ * @author Md. Golam Mostafa | mostafa.sna@gmail.com
+ * @File com.mostafa.ThreadLifeCycle.Chef.java: JavaParallelProgramming
+ * @CreationDate 6/28/2022 12:19 PM
+ */
+public class Chef implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Helper Chef started & waiting for sausage to thaw...");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Helper Chef is done cutting sausage.");
+    }
+}
